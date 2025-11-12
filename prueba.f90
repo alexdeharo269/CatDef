ea22acb3409c7fbfbda53f0a77e9d3283ab4c9f9
germanrojo02 <germangonzalezrojo@gmail.com>
2025-11-12 18:25:02 +0100

    hoola

---
+++ prueba.f90
@@ -0,0 +1,14 @@
+program prueba
+
+    implicit none
+    integer :: i
+    real :: x
+    
+    x = 0.0
+    do i = 1, 10
+         x = x + 1.0 / real(i)
+    end do
+    
+    print *, "La suma de los primeros 10 términos de la serie armónica es:", x
+    
+end program prueba
